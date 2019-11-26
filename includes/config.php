98fd21768c30c6ba634715cf6d13cede677c2252
Abdul Halim <43396244+HahaHalim@users.noreply.github.com>
2019-11-26 23:04:06 +0800

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
-$dbuser="root";
-$dbpass="";
-$host="localhost";
-$db="gym4";
+$dbuser="b61024b9978bf4";
+$dbpass="1da2513a";
+$host="us-cdbr-iron-east-05.cleardb.net";
+$db="heroku_495aaa2cb1fa4bb";
 $mysqli =new mysqli($host,$dbuser, $dbpass, $db);
 ?>
