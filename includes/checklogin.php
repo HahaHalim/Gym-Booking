7e5751fd72d4176733dfcb130b85d3b72f4becc2
Abdul Halim <43396244+HahaHalim@users.noreply.github.com>
2019-11-27 01:34:06 +0800

    Update checklogin.php

--- includes/checklogin.php
+++ includes/checklogin.php
@@ -4,7 +4,7 @@ function check_login()
 if(strlen($_SESSION['id'])==0)
 	{	
 		$host = $_SERVER['us-cdbr-iron-east-05.cleardb.net'];
-		$uri  = rtrim(dirname($_SERVER['us-cdbr-iron-east-05.cleardb.net']), '/\\');
+		$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 		$extra="index3.php";		
 		$_SESSION["id"]="";
 		header("Location: http://$host$uri/$extra");
